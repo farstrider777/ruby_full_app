cba3cb925318d8fb782788877e6fa530d58038f8
Joshua Bradford <jjyet@yahoo.com>
2017-05-21 16:29:22 -0400

    added function to aplicationHelper that will still pass all tests.. now more D.R.Y

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,7 +12,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@app_name}"
+    assert_select "title", "#{@app_name}"
   end
 
   test "should get help" do
